856cf8e525c2fb82d4d84172c980db20464f6759
Burton Johnsey <git@tonjohn.com>
2020-05-22 06:40:37 -0700

    remove space in title added by formatter

--- tonjohn-dot-com/src/pages/Index.vue
+++ tonjohn-dot-com/src/pages/Index.vue
@@ -145,9 +145,7 @@
               <h2
                 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl"
               >
-                bur
-                <span class="text-indigo-600">tonjohn</span>sey
-                <br class="xl:hidden" />
+                bur<span class="text-indigo-600">tonjohn</span>sey
               </h2>
               <p
                 class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
